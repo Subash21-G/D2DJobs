711c9d1b5e3d60ddc173b708aa711b35c332a244
Subash G <subashgbose21@gmail.com>
2026-09-23 12:45:08 +0530

    Add Monetag service worker

--- wwwroot/sw.js
+++ wwwroot/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11852095
+    "zoneId": 11867691
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
