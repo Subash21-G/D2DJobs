d406f8ff0910ae04f80c0036d05efd87b52a0d6f
Subash G <subashgbose21@gmail.com>
2026-09-23 12:43:49 +0530

    Install Monetag zone 284842 tag

--- Models/MonetagOptions.cs
+++ Models/MonetagOptions.cs
@@ -4,5 +4,6 @@ public class MonetagOptions
 {
     public bool Enabled { get; set; }
     public string ZoneId { get; set; } = "";
-    public bool IsReady => Enabled && Regex.IsMatch(ZoneId, "^[0-9]{5,12}$");
+    public string ScriptUrl { get; set; } = "https://auge5.com/88/tag.min.js";
+    public bool IsReady => Enabled && Uri.TryCreate(ScriptUrl, UriKind.Absolute, out var uri) && uri.Scheme == "https" && Regex.IsMatch(ZoneId, "^[0-9]{5,12}$");
 }
